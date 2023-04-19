3060e5d49eb635061bbaee38316e8e57dab1bcdf
houndxds <david.viitanen@gmail.com>
2023-04-19 11:02:57 +0200

    Added more shit

---
+++ video.js
@@ -0,0 +1,19 @@
+document.getElementById("passive").addEventListener("click", function() {
+    document.getElementById("video-passive").src = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0053/ability_0053_P1.webm";
+})
+
+document.getElementById("q-ability").addEventListener("click", function() {
+    document.getElementById("video-passive").src = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0053/ability_0053_Q1.webm";
+})
+
+document.getElementById("w-ability").addEventListener("click", function() {
+    document.getElementById("video-passive").src = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0053/ability_0053_W1.webm";
+})
+
+document.getElementById("e-ability").addEventListener("click", function() {
+    document.getElementById("video-passive").src = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0053/ability_0053_E1.webm";
+})
+
+document.getElementById("r-ability").addEventListener("click", function() {
+    document.getElementById("video-passive").src = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0053/ability_0053_R1.webm";
+})
